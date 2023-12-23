af601c2d56bd73659c83f2cffa18946387ba06cd
elias-a <elias.alstead@gmail.com>
2023-12-23 17:49:12 -0500

    Change default indent to 2 spaces and set indent to 4 spaces for Python files

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,9 +1,9 @@
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
@@ -17,3 +17,8 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "py",
+  command = "setlocal shiftwidth=4 tabstop=4 softtabstop=4"
+})
