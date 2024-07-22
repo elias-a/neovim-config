4a5f294fc70a05a43311378daee30f08052de6a7
elias-a <elias.alstead@gmail.com>
2024-07-22 13:27:50 -0400

    Do not automatically try Flake8 linter

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -5,6 +5,5 @@ require("lint").linters_by_ft = {
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
    callback = function()
       require("lint").try_lint()
-      require("lint").try_lint("Flake8")
    end,
 })
