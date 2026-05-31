f3e248df29467d911f1075e91360326f0dbe1053
elias-a <elias.alstead@gmail.com>
2026-05-31 13:25:12 -0400

    Update nvim-treesitter config and do not install nvim-treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,83 @@
 return {
    {
       "nvim-treesitter/nvim-treesitter",
+      lazy = false,
       build = ":TSUpdate",
+      event = { "BufReadPre", "BufNewFile" },
+      init = function()
+         local highlight = function(bufnr, lang)
+            if not vim.treesitter.language.add(lang) then
+               return vim.notify(
+                  string.format("treesitter cannot load parser for language: %s", lang),
+                  vim.log.levels.INFO,
+                  { title = "treesitter" }
+               )
+            end
+            vim.treesitter.start(bufnr)
+         end
 
-      config = function()
-         require("nvim-treesitter").setup({
-            ensure_installed = {
-               "vimdoc",
-               "lua",
-               "javascript",
-               "typescript",
-               "c",
-               "python",
-            },
+         vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+               local ft = vim.bo.filetype
+               local bt = vim.bo.buftype
+               local buf = args.buf
 
-            sync_install = false,
-            auto_install = true,
+               if bt ~= "" then
+                  return
+               end
 
-            highlight = {
-               enable = true,
-               additional_vim_regex_highlighting = false,
-            },
+               local ok, treesitter = pcall(require, "nvim-treesitter")
+               if not ok then
+                  return
+               end
+
+               if vim.fn.executable "tree-sitter" ~= 1 then
+                  vim.api.nvim_echo({
+                     {
+                        "tree-sitter not found. Parsers cannot be installed.",
+                        "ErrorMsg",
+                     },
+                  }, true, {})
+                  return false
+               end
+
+               if not vim.treesitter.language.get_lang(ft) then
+                  return
+               end
+
+               if vim.list_contains(treesitter.get_installed(), ft) then
+                  highlight(buf, ft)
+               elseif vim.list_contains(treesitter.get_available(), ft) then
+                  treesitter.install(ft):await(function()
+                     highlight(buf, ft)
+                  end)
+               end
+            end,
          })
       end,
-   },
-
-   {
-      "nvim-treesitter/nvim-treesitter-context",
+      opts = {
+         install = {
+            "vimdoc",
+            "lua",
+            "javascript",
+            "typescript",
+            "c",
+            "python",
+         },
+      },
+      config = function(_, opts)
+         local treesitter = require "nvim-treesitter"
+         treesitter.setup(opts)
+         if vim.fn.executable "tree-sitter" ~= 1 then
+            vim.api.nvim_echo({
+               {
+                  "tree-sitter not found. Parsers cannot be installed.",
+                  "ErrorMsg",
+               },
+            }, true, {})
+            return false
+         end
+         treesitter.install(opts.install)
+      end,
    },
 }
